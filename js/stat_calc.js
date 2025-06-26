7afd5763554166b41634ff357fd351b6024673b5
JoshuaKaiel <joshukaiel@gmail.com>
2025-06-26 13:03:01 +0200

    I'm trying

--- js/stat_calc.js
+++ js/stat_calc.js
@@ -1,7 +1,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-import { getFirestore, collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js"
+import { getFirestore, getDocs, collection, query, where } from 'https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
